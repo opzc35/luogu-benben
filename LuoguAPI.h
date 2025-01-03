9b36e0ed0ff722b9007802dc672a16727248e1c5
opzc35 <opzc35@outlook.com>
2025-01-03 19:24:44 +0800

    Update LuoguAPI.h

--- LuoguAPI.h
+++ LuoguAPI.h
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <curl/curl.h>
+#include <curl/curl.h> 
 
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
     ((std::string*)userp)->append((char*)contents, size * nmemb);
